602623a276ded3fa7fbceec9d45c07844324c378
Artemis-Fox <105129173+Artemis-Fox@users.noreply.github.com>
2022-11-26 14:35:46 -0700

    Add files via upload

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-/usr/lib/xfce-polkit/xfce-polkit &
-picom -b --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 350 &
-udiskie &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+picom -b --animations-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 350 &
+udiskie & 
 nitrogen --restore &
-dunst &
 xrdb ~/.Xresources &
 flameshot &
