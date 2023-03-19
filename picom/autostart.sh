cc023c5e2f641a49dfafcb2e010270e29426e0cc
Artemis-Fox <105129173+Artemis-Fox@users.noreply.github.com>
2023-03-19 14:12:28 -0600

    Delete autostart.sh

--- picom/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/lib/xfce-polkit/xfce-polkit &
-picom -b --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 350 &
-udiskie &
-nitrogen --restore &
-dunst &
-xrdb ~/.Xresources &
-flameshot &
