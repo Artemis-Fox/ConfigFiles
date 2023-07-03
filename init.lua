35257c0ac87233e51a9dfcd6858b9851c6687d2e
Artemis-Fox <105129173+Artemis-Fox@users.noreply.github.com>
2023-07-03 10:02:36 -0600

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- turn on line numbers
+vim.o.number = true
+
+-- enable mouse support
+vim.o.mouse = a
+
+-- turn on syntax highlighting
+vim.o.syntax = ON
+
+-- turn on smart indent
+vim.o.smartindent = true
+
+-- turn on the spell checker
+vim.o.spell = true
