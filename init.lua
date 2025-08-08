b0002cd3357a9038b7a75ab1a0ec584f5ed6ff10
Artemis-Fox <105129173+Artemis-Fox@users.noreply.github.com>
2025-08-08 10:52:10 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -12,3 +12,9 @@ vim.o.smartindent = true
 
 -- turn on the spell checker
 vim.o.spell = true
+
+-- Transparent Background
+vim.api.nvim_set_hl(0, "Normal", { bg = nil})
+
+-- Yellow line numbers
+vim.api.nvim_set_hl(0, "LineNr", { fg = "Yellow"})
